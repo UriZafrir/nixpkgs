aa307a4fd4c88cce0b3b65d762bfd82c4136c3f2
uri zafrir <urizaf@gmail.com>
2025-09-22 15:37:58 +0300

    continue

--- pkgs/by-name/wi/witsy/package.nix
+++ pkgs/by-name/wi/witsy/package.nix
@@ -5,6 +5,9 @@
   electron,
   makeDesktopItem,
   makeWrapper,
+  nodejs,
+  nodePackages,
+  jq,
 }:
 
 buildNpmPackage rec {
@@ -18,14 +21,44 @@ buildNpmPackage rec {
     hash = "sha256-YW07wBx5Ybf+87gTY6QhxuK772kNo64qqED9RCPa/uw=";
   };
 
-  npmDepsHash = "sha256-6l8Tgd9wxtnTgjNktBIO/b9lDJhi72vMePdwLGctlSM=";
+  npmDepsHash = "sha256-Tqh63RC9Q64KdHFT7DW3IJWviM/pRVLLYqycHskheGQ=";
+
+  nativeBuildInputs = [
+    nodejs
+  ];
 
   forceGitDeps = true;
 
   makeCacheWritable = true;
 
+  npmFlags = [ "--ignore-scripts" ];
+
+  postPatch = ''
+    # Remove the problematic Git dependency @vue/test-utils which has install scripts requiring rollup
+    # This is a dev dependency used for testing and not needed for the build
+    ${jq}/bin/jq 'del(.devDependencies."@vue/test-utils")' package.json > package.json.tmp
+    mv package.json.tmp package.json
+    ${jq}/bin/jq '
+      del(.devDependencies."@vue/test-utils") |
+      walk(
+        if type == "object" and has("packages") then
+          .packages |= with_entries(select(.key | contains("@vue/test-utils") | not))
+        else .
+        end
+      )
+    ' package-lock.json > package-lock.json.tmp
+    mv package-lock.json.tmp package-lock.json
+
+    # Set electronDist in forge.config.ts to use Nix electron
+    sed -i 's|const config: ForgeConfig = {|const config: ForgeConfig = {\n  electronDist: process.env.electronDist,|' forge.config.ts
+  '';
+
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+  env.electronDist = "${electron.dist}";
+
   configurePhase = ''
     export npm_config_offline="false"
+    export npm_config_ignore_scripts="true"
   '';
 
   buildInputs = [ electron ];
