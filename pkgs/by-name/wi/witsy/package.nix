5a3b438fd059f6bfbcc825873c3a72a1c3dc5240
uri zafrir <urizaf@gmail.com>
2025-09-22 14:48:16 +0300

    add package.nix

---
+++ pkgs/by-name/wi/witsy/package.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+  electron,
+  makeDesktopItem,
+  makeWrapper,
+}:
+
+buildNpmPackage rec {
+  pname = "witsy";
+  version = "2.14.0";
+
+  src = fetchFromGitHub {
+    owner = "nbonamy";
+    repo = "witsy";
+    tag = "v${version}";
+    hash = "sha256-YW07wBx5Ybf+87gTY6QhxuK772kNo64qqED9RCPa/uw=";
+  };
+
+  npmDepsHash = "sha256-6l8Tgd9wxtnTgjNktBIO/b9lDJhi72vMePdwLGctlSM=";
+
+  forceGitDeps = true;
+
+  buildInputs = [ electron ];
+
+  npmBuildScript = "package";
+
+  npmFlags = [ "--cache /tmp/npm-cache" ];
+
+  desktopItem = makeDesktopItem {
+    name = "witsy";
+    exec = "witsy %U";
+    icon = "witsy";
+    desktopName = "Witsy";
+    genericName = "LLM Chat App";
+    comment = "Interact with LLMs";
+    categories = [ "Utility" ];
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/share/witsy $out/share/applications
+
+    # Copy the built app
+    cp -r out/Witsy-linux-x64/resources/app $out/share/witsy/
+
+    # Wrap electron
+    makeWrapper ${electron}/bin/electron $out/bin/witsy \
+      --add-flags $out/share/witsy \
+      --set ELECTRON_IS_PACKAGED 1
+
+    # Desktop file
+    cp ${desktopItem}/share/applications/* $out/share/applications/
+
+    # TODO: add icon if available
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "An app to interact with LLMs";
+    homepage = "https://github.com/nbonamy/witsy";
+    license = lib.licenses.mit;
+    maintainers = [ ]; # TODO: add maintainer
+    platforms = electron.meta.platforms;
+    mainProgram = "witsy";
+  };
+}
